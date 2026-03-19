fe091d8b6c07452d91a5ee1a0ca5d6403578f7b8
Luana Maria de Santana Souza <luana_dark34@hotmail.com>
2026-03-19 11:01:07 -0300

    listas DropDown header

---
+++ js/header.js
@@ -0,0 +1,73 @@
+class DropDown {
+
+    constructor() {
+
+        this.elementoDeClick = document.querySelector("#dropLista");
+        this.elementoDeClick2 = document.querySelector("#elementoClick2");
+        this.navColagem = document.querySelector(".nav");
+
+        this.criarDropdowns();
+        this.caixaDeLinks();
+    }
+
+    criarDropdowns() {
+        this.dropdown1 = document.createElement("div");
+        this.dropdown1.classList.add("dropdown");
+        this.dropdown1.style.display = "none";
+        this.dropdown1.innerHTML = `
+            <ul class="listaDrop">
+                <li><a href="">GESTÃO E COMERCIO</a></li>
+                <li><a href="">CULTURA</a></li>
+                <li><a href="">APRENDIZAGEM</a></li>
+                <li><a href="">PERFIL</a></li>
+                <li><a href="">SAUDE</a></li>
+                <li><a href="">TECNOLOGIA</a></li>
+            </ul>
+        `;
+
+        this.dropdown2 = document.createElement("div");
+        this.dropdown2.classList.add("dropdown", "lista2");
+        this.dropdown2.style.display = "none";
+        this.dropdown2.innerHTML = `
+            <ul class="listaDrop">
+                <li style="margin-top: 8px;"><a href="">GESTÃO E COMERCIO</a></li>
+                <li><a href="">CULTURA</a></li>
+                <li style="margin: 0;"><a href="">APRENDIZAGEM</a></li>
+            </ul>
+        `;
+
+        this.navColagem.appendChild(this.dropdown1);
+        this.navColagem.appendChild(this.dropdown2);
+    }
+
+    caixaDeLinks() {
+
+        this.elementoDeClick.addEventListener("click", e => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.toggle(this.dropdown1);
+        });
+
+        this.elementoDeClick2.addEventListener("click", e => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.toggle(this.dropdown2);
+        });
+
+        document.addEventListener("click", () => {
+            this.fecharTodos();
+        });
+    }
+
+    toggle(dropdown) {
+        this.fecharTodos();
+        dropdown.style.display = "block";
+    }
+
+    fecharTodos() {
+        this.dropdown1.style.display = "none";
+        this.dropdown2.style.display = "none";
+    }
+}
+
+new DropDown();
